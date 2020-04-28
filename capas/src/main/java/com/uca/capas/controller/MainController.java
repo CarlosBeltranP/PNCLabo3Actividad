afd54fab336e3576e7e302ba44201761d6e3fbfe
CarlosBeltranP <00507511@uca.edu.sv>
2020-04-28 11:44:18 -0600

    creacion de constructor de clase Student y ejemplo2 en MainController

--- capas/src/main/java/com/uca/capas/controller/MainController.java
+++ capas/src/main/java/com/uca/capas/controller/MainController.java
@@ -1,15 +1,31 @@
 package com.uca.capas.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.uca.capas.domain.Student;
+
 @Controller
 public class MainController {
 	
-	@GetMapping ("/ejemplo1")
+	@GetMapping (path = "/ejemplo1", produces = MediaType.TEXT_PLAIN_VALUE)
 	@ResponseBody
 	public String ejemplo1() {
 		return "Bienvenidos\n" + "Programación N-Capas";
+	}
+	
+	@GetMapping("/ejemplo2") //@ResponseBody puede devolver Jason
+	public @ResponseBody List<Student> ejemplo2(){
+		return Arrays.asList( //lista de estudiantes 
+				new Student("Nombre1", "Apellido", "10/10/1992", "Carrera X ", true),
+				new Student("Nombre1", "Apellido", "10/10/1992", "Ingeniería en Limones", false)
+				);
+				
+		
 	}
 }
